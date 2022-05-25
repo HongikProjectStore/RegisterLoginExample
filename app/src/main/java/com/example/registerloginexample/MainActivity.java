77369bc8e775ad6bab0395028c663fe53437771d
hijw <pattyyoon@g.hongik.ac.kr>
2022-05-26 00:14:06 +0900

    4th commit

--- app/src/main/java/com/example/registerloginexample/MainActivity.java
+++ app/src/main/java/com/example/registerloginexample/MainActivity.java
@@ -1,29 +1,86 @@
 package com.example.registerloginexample;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
-public class  MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
-    private TextView tv_id, tv_pass;
+    private DrawerLayout drawerLayout;
+    private View drawerView;
+    // alt+enter 해서 import android.view.View 넣기 필요
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tv_id = findViewById(R.id.tv_id);
-        tv_pass = findViewById(R.id.tv_pass);
+
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        drawerView =(View)findViewById(R.id.drawer);
 
 
-        Intent intent = getIntent();
-        String userID = intent.getStringExtra("userID");
-        String userPass = intent.getStringExtra("userPass");
 
-        tv_id.setText(userID);
-        tv_pass.setText(userPass);
+        Button btn_open = (Button)findViewById(R.id.btn_open);
+        btn_open.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(drawerView);
+            }
+        });
+
+
+
+
+        // close 버튼 눌렀을때 행동을 취해라
+        Button btn_close = (Button)findViewById(R.id.btn_close);
+        btn_close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.closeDrawers();
+            }
+        });
+
+
+        drawerLayout.setDrawerListener(listener);
+        drawerView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                return true;
+            }
+        });
 
     }
+
+
+    //특정 action에서 추가기능 하고싶으면 넣으면 된다.
+    DrawerLayout.DrawerListener listener = new DrawerLayout.DrawerListener() {
+        @Override
+        public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+            // 슬라이드 했을때
+        }
+
+        @Override
+        public void onDrawerOpened(@NonNull View drawerView) {
+            // open 완료했을때
+        }
+
+        @Override
+        public void onDrawerClosed(@NonNull View drawerView) {
+
+        }
+
+        @Override
+        public void onDrawerStateChanged(int newState) {
+            // 상태 change 되었을때
+        }
+    };
+
+
 }
